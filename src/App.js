a7725f3a772c22fdb9ead11cecb08883e106a7f4
Aman Singh <amansinghrathhore117@gmail.com>
2021-03-13 17:14:17 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import './style.css';
+ 
+function App()  {
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* <div id="stars" />
+      <div id="stars2" />
+      <div id="stars3" />  */}
+    
+      <h1 className=" logo"> Mangalyaan </h1>  
+      
+     
+      
+
+       
+       {/* <img src="https://concetto-front.s3.ap-south-1.amazonaws.com/moon_surface.webp" className="jss4" alt=" " ></img> */}
+      
     </div>
+  
   );
 }
 
